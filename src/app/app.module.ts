063e85748ffb681f640e4e6a6452ca571b0776b8
amirhoseinh73 <amirhoseinh1373@gmail.com>
2023-12-04 01:18:10 +0100

    Install PWA, Firebase Hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core"
+import { NgModule, isDevMode } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 
 import { AppRoutingModule } from "./app-routing.module"
@@ -7,7 +7,8 @@ import { SquareComponent } from "./square/square.component"
 import { BoardComponent } from "./board/board.component"
 
 import { NbButtonModule, NbLayoutModule, NbThemeModule } from "@nebular/theme"
-import { NbEvaIconsModule } from "@nebular/eva-icons"
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import { ServiceWorkerModule } from '@angular/service-worker'
 
 @NgModule({
   declarations: [AppComponent, SquareComponent, BoardComponent],
@@ -18,6 +19,12 @@ import { NbEvaIconsModule } from "@nebular/eva-icons"
     NbLayoutModule,
     NbEvaIconsModule,
     NbButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
